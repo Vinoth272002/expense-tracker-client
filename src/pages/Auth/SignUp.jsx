bfff491609cb38ded8b9abab08792068852fb01e
vinoth <vinoth27172002@gmail.com>
2026-02-14 20:44:41 +0530

    feat(auth): enhance SignUp component with image upload functionality

--- src/pages/Auth/SignUp.jsx
+++ src/pages/Auth/SignUp.jsx
@@ -1,23 +1,25 @@
 import { useState } from "react";
 import AuthLayout from "../../components/layouts/AuthLayout";
 import Input from "../../components/inputs/Input";
-import {Link, useNavigate} from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import isEmail from 'validator/lib/isEmail';
 import ProfilePhotoSelector from "../../components/inputs/ProfilePhotoSelector";
+import useAuth from "../../store/useAuth";
+import uploadImage from "../../utils/uploadImage";
 
 const SignUp = () => {
-    const [ profilePic, setProfilePic ] = useState(null);
-    const [ fullName, setFullName ] = useState("");
-    const [ email, setEmail ] = useState("");
-    const [ password, setPassword ] = useState(""); 
-    const [ error, setError ] = useState("");
+    const [profilePic, setProfilePic] = useState(null);
+    const [fullName, setFullName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
 
+    const { signup } = useAuth();
     const navigate = useNavigate();
 
-    const handleSignUp = (event) => {
+    const handleSignUp = async (event) => {
         event.preventDefault();
 
-        let profileImageUrl = "";
         if (!fullName.length) {
             setError("Pleae enter your name");
             return;
@@ -28,39 +30,55 @@ const SignUp = () => {
             return;
         }
         
-        if(!password.length) {
+        if (!password.length) {
             setError("please enter the password");
             return;
         }
 
         setError("");
 
+        try {
+            let profilePicUrl = "";
+
+            if (profilePic) {
+                const imageUploadRes = await uploadImage(profilePic);
+                profilePicUrl = imageUploadRes.imageUrl || "";
+            }
+
+            await signup({ fullName, email, password, profilePicUrl: profilePicUrl });
+            navigate("/dashboard");
+        } catch (error) {
+            if (error.response?.message) {
+                setError(error.response.message)
+            } else {
+                setError("An unexpected error occured. Please try again.");
+            }
+        }
     }
     return (
         <AuthLayout>
             <div className="lg:w-[100%] h-auto md:h-full mt-10 md:mt-0 flex flex-col justify-center">
                 <h3 className="text-xl font-semibold text-black">Create an Account</h3>
-                <p  className="text-xs text-slate-700 mt-[5px] mb-6 ">Join us today by entering your details below</p>
+                <p className="text-xs text-slate-700 mt-[5px] mb-6 ">Join us today by entering your details below</p>
 
-                <form onSubmit={ handleSignUp }>
+                <form onSubmit={handleSignUp}>
 
-                    <ProfilePhotoSelector image={ profilePic } setImage={ setProfilePic }></ProfilePhotoSelector>
+                    <ProfilePhotoSelector image={profilePic} setImage={setProfilePic}></ProfilePhotoSelector>
 
                     <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-                        <Input value={ fullName } onChange={ ({ target }) => setFullName(target.value)} label="Full Name" placeholder="Tracker" type="text"></Input>
-                        <Input value={ email } onChange={ ({ target }) => setEmail(target.value)} label="Email" placeholder="expensetracker@gmail.com" type="text"></Input>
+                        <Input value={fullName} onChange={({ target }) => setFullName(target.value)} label="Full Name" placeholder="Tracker" type="text"></Input>
+                        <Input value={email} onChange={({ target }) => setEmail(target.value)} label="Email" placeholder="expensetracker@gmail.com" type="text"></Input>
 
                         <div className="col-span-2">
-                            <Input value={ password } onChange={ ({ target }) => setPassword(target.value)} label="Password" placeholder="Min 8 charecters" type="password"></Input>
+                            <Input value={password} onChange={({ target }) => setPassword(target.value)} label="Password" placeholder="Min 8 charecters" type="password"></Input>
                         </div>
                     </div>
 
-                    { error && <p className="text-red-500 text-xs pb-2.5">{ error }</p>}
-
+                    {error && <p className="text-red-500 text-xs pb-2.5">{error}</p>}
                     <button type="submit" className="btn-primary">SIGN UP</button>
 
                     <p className="text-[13px] text-slate-800 mt-3">
-                        Already have an account?{ " " }
+                        Already have an account?{" "}
                         <Link to="/login" className="font-medium text-primary underline">Login</Link>
                     </p>
                 </form>
